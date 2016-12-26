f0b6669a869f197fd3cb15337c07e822d4a5e96f
Andrew Lee <andrew.warren.lee@gmail.com>
2016-12-25 21:09:36 -0800

    added some directory traversal

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import glob
+
+if __name__ == '__main__':
+    proj = sys.argv[1]
+    path = proj + '/app/controllers'
+    query = sys.argv[2]
+    for filename in glob.glob(os.path.join(path, '*.rb')):
+        with open(filename, 'r') as f:
+            print(f.read())
+            print(filename)
