a82c64c7eaf7024e9a52c92f360174ff5f060cbd
Andrew Lee <andrew.warren.lee@gmail.com>
2017-01-05 14:02:59 -0800

    recursive search added

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 import sys
 import glob
 
+def views_search(query, view_path):
+    for cur, _dirs, files in os.walk(view_path):
+        for dir in _dirs:
+            views_search(query, dir)
+        for file in files:
+            f = open(cur + '/' + file)
+            if query in f.read():
+               print(cur + '/' + file)
+
 if __name__ == '__main__':
     proj = sys.argv[1]
-    path = proj + '/app/controllers'
+    path = proj + '/app/views/'
     query = sys.argv[2]
-    for filename in glob.glob(os.path.join(path, '*.rb')):
-        with open(filename, 'r') as f:
-            if query in f.read():
-                print(filename)
+    views_search(query, path)
 
-#write recursive folder traversal
